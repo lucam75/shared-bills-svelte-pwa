f57439454325bd32b1f4feff4d049f6b584fd9b5
Luis Campos <lucam75@gmail.com>
2020-09-07 22:02:56 -0300

    Updating redirectUri

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ export const oauth2 = new jsforce.OAuth2({
     loginUrl : 'https://cunning-fox-tqgyk9-dev-ed.my.salesforce.com',
     clientId : '3MVG95jctIhbyCprKkuB.DbXd55yOtTF5JrNA.EZ6iG8K_pWiNF7G6EPN_iXEwHdSW83jUE6Kux._DJZ.HihM',
     clientSecret : '1F5E3DA5394BFF18B9DFD9D9D9C1B71C2ECC8E09CA2DB1C6129067653E76C21C',
-    redirectUri : 'http://localhost:5000/#/token',
+    redirectUri : 'https://lucam75.github.io/shared-bills-svelte-pwa/',
     version: '49.0',
     proxyUrl: 'https://shared-bills-proxy.herokuapp.com/proxy'
 });
