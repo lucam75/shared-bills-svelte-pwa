b6a4c01b4ebe9fea81244ac393c4a77b24d7ab1f
Luis Campos <lucam75@gmail.com>
2020-09-13 12:37:16 -0300

    Updating service worker

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -29,8 +29,8 @@ self.addEventListener("activate", e => {
 self.addEventListener("fetch", e => {
   e.respondWith(
     (async function() {
-      const response = await caches.match(e.request);
-      return response || fetch(e.request);
+      // const response = await caches.match(e.request);
+      return fetch(e.request);
     })()
   );
 });
